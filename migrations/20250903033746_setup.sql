e53e31c6208f5ec3fba2b1cadc586cc21895e30a
luizHOAlves <luiz.alves@sempreceub.com>
2026-04-09 17:22:40 -0300

    creating new table PERMISSIONS and adding FK into USERS table

--- migrations/20250903033746_setup.sql
+++ migrations/20250903033746_setup.sql
@@ -9,7 +9,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
+    password_hash TEXT NOT NULL, 
     permission INT REFERENCES permissions(id)
 );
 
