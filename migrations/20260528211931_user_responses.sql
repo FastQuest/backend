70303f7c0258092a3364187c1999998cd1d03216
Anthony Passos <anthony.passos@sempreceub.com>
2026-05-28 20:43:22 -0300

    tabela de respostas e tentativas/envios de resoluções de listas

---
+++ migrations/20260528211931_user_responses.sql
@@ -0,0 +1,42 @@
+-- +goose Up
+-- +goose StatementBegin
+DROP TABLE IF EXISTS user_response;
+
+CREATE TABLE submission (
+    id SERIAL PRIMARY KEY,
+    question_set_id INT REFERENCES question_set(id),
+    user_id INT REFERENCES users(id),
+    hits INT NOT NULL,
+    answers_count INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE response (
+    id SERIAL PRIMARY KEY,
+    submission_id INT REFERENCES submission(id),
+    question_id INT REFERENCES question(id),
+    answer_id INT REFERENCES answer(id),
+    is_correct BOOLEAN NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+CREATE TABLE user_response (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    question_id INT NOT NULL REFERENCES question(id),
+    question_set_id INT NOT NULL REFERENCES question_set(id),
+    answer_id INT NOT NULL REFERENCES answer(id),
+    is_correct BOOLEAN NOT NULL,
+    answered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS response;
+DROP TABLE IF EXISTS submission;
+-- +goose StatementEnd
