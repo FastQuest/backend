1e54317a24769851857a2fdc08a91eafccc19bf4
Anthony Passos <anthony.passos@sempreceub.com>
2026-06-05 07:10:37 -0300

    atualizando

--- internal/answer/dto.go
+++ internal/answer/dto.go
@@ -1,5 +1,7 @@
 package answer
 
+import "flashquest/pkg/models"
+
 type CreateAnswerRequest struct {
 	SubmissionID     uint `gorm:"not null" json:"submission_id"`
 	QuestionID       uint `gorm:"not null" json:"question_id"`
@@ -8,16 +10,8 @@ type CreateAnswerRequest struct {
 }
 
 type SubjectPerformance struct {
-	SubjectID         int     `gorm:"column:subject_id" json:"subject_id"`
-	TotalAnswers      int     `gorm:"column:total_answers" json:"total_answers"`
-	TotalCorrect      int     `gorm:"column:total_correct" json:"total_correct"`
-	PercentualCorrect float64 `gorm:"column:percentual_correct" json:"percentual_correct"`
-}
-
-type SubjectPerformanceResponse struct {
-	UserID            int     `json:"user_id"`
-	SubjectID         int     `json:"subject_id"`
-	TotalAnswers      int     `json:"total_answers"`
-	TotalCorrect      int     `json:"total_correct"`
-	PercentualCorrect float64 `json:"percentual_correct"`
+	Subject           models.Subject `gorm:"embedded" json:"subject"`
+	TotalAnswers      int            `gorm:"column:total_answers" json:"total_answers"`
+	TotalCorrect      int            `gorm:"column:total_correct" json:"total_correct"`
+	PercentualCorrect float64        `gorm:"column:percentual_correct" json:"percentual_correct"`
 }
--- internal/answer/repository.go
+++ internal/answer/repository.go
@@ -19,16 +19,17 @@ func GetUserPerfomace(db *gorm.DB, userID int) ([]SubjectPerformance, error) {
 
 	err := db.Table("submission s").
 		Select(`
-			q.subject_id,
-			COUNT(a.id) AS total_answers,
-			SUM(CASE WHEN a.is_correct THEN 1 ELSE 0 END) AS total_correct,
-			ROUND((SUM(CASE WHEN a.is_correct THEN 1 ELSE 0 END) * 100.0) / COUNT(a.id), 2) AS percentual_correct
-		`).
+            sub.*,
+            COUNT(a.id) AS total_answers,
+            SUM(CASE WHEN a.is_correct THEN 1 ELSE 0 END) AS total_correct,
+            ROUND((SUM(CASE WHEN a.is_correct THEN 1 ELSE 0 END) * 100.0) / COUNT(a.id), 2) AS percentual_correct
+        `).
 		Joins("JOIN answer a ON a.submission_id = s.id").
 		Joins("JOIN question q ON a.question_id = q.id").
+		Joins("JOIN subject sub ON q.subject_id = sub.id").
 		Where("s.user_id = ?", userID).
 		Where("s.deleted_at IS NULL AND a.deleted_at IS NULL").
-		Group("q.subject_id").
+		Group("sub.id"). // Agrupamos pela Primary Key do subject
 		Order("percentual_correct ASC").
 		Scan(&performances).Error
 
